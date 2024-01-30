558ad38d47ace34ab66355ed146dee20a26fa087
Quang Phan <oclockvn@gmail.com>
2024-01-30 15:56:07 +0700

    add movie service

---
+++ FilmLibrary.Lib/Models/MovieModel.cs
@@ -0,0 +1,11 @@
+﻿namespace FilmLibrary.Lib.Models
+{
+    public class MovieModel
+    {
+        public string Title { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public string Director { get; set; } = null!;
+        public int IMDBRating { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
